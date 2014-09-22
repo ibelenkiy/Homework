02ba9e95ae1fa73b4c671150c1d57b310c367dd6
ibelenkiy <ibelenkiy@aol.com>
2014-09-21 23:40:24 -0400

    Homework 3

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args){
+        String s="Isaac Belenkiy";
+        Position = s.indexOf(" ");
+        Seperated = String[] s.split(" " , 1) 
+	    System.out.println("The original string was ... " + s)
+	    System.out.println("The position of the space is ... " + Position)
+	    System.out.println("The two seperated names are ... " + Seperated)
+    }
+}
